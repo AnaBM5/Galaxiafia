30f763199ce05e561795ee2e9bea4af5c77c04b9
AnaBM5 <anabmojica@hotmail.com>
2020-03-07 11:42:21 -0500

    initial commit

---
+++ Proyecto/Assets/Scripts/ShowLives.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowLives : MonoBehaviour
+{
+    public static int livesAmount = 3;
+    private TextMeshProUGUI lives;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives.text = "Vidas: " + livesAmount.ToString();
+    }
+}
