3420faeb0bbc4db5c3309846730f54cbe54c4bf2
AnaBM5 <anabmojica@hotmail.com>
2020-03-07 11:56:49 -0500

    game commit

---
+++ Proyecto/Assets/Scripts/CanvasToWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasToWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform recTransform;
+    void Start()
+    {
+        recTransform = GetComponent<Transform>();
+       Camera.main.ScreenToWorldPoint(recTransform.transform.position); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CanvasPosition()
+    {
+        
+    }
+}
---
+++ Proyecto/Assets/Scripts/ChooseOption.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseOption : MonoBehaviour
+{
+    private Vector2 initialPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("RightOption"))
+        {
+            RightAnswer();
+        }else if (other.CompareTag("WrongOption"))
+        {
+            WrongAnswer();
+        }
+    }
+
+    void RightAnswer()
+    {
+        ShowScore.scoreValue += 10;
+        transform.position = initialPosition;
+
+    }
+
+    void WrongAnswer()
+    {
+        ShowLives.livesAmount--;
+        transform.position = initialPosition;
+        if (ShowLives.livesAmount <= 0)
+        {
+            KillPlayer();
+        }
+    }
+
+    void KillPlayer()
+    {
+        Destroy(this.gameObject);
+    }
+}
