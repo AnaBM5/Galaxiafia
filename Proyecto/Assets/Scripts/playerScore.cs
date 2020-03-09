f90426488ece67ca3d6c26c9996a6bd0c0498adb
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2020-03-09 10:03:35 -0500

    Add score to bd

---
+++ Proyecto/Assets/Scripts/playerScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class playerScore : MonoBehaviour
+{
+    public Text scoreText;
+    public static int score;
+    public static string playerName;
+    user user = new user();
+    private System.Random random= new System.Random();
+    public InputField nameText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = random.Next(1000);
+        scoreText.text="puntaje: "+score;
+    }
+    public void OnSumit()
+    {
+        playerName = nameText.text;
+        PostToDataBase();
+    }
+    private void PostToDataBase()
+    {
+        user user = new user();
+        RestClient.Put("https://usuarios-ff410.firebaseio.com/" + playerName + ".json", user);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
