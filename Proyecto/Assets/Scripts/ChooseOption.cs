14bc26a3cd77d10c6bf15318220b2887ecd456e9
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2020-04-14 14:26:29 -0500

    automatic change question

--- Proyecto/Assets/Scripts/ChooseOption.cs
+++ Proyecto/Assets/Scripts/ChooseOption.cs
@@ -10,10 +10,12 @@ public class ChooseOption : MonoBehaviour
     private Vector2 wrongAnswer;
     public GameObject perdiste;
     public GameObject principal;
+    private PlayerText nextQuestions;
 
     // Start is called before the first frame update
     void Start()
     {
+        nextQuestions = FindObjectOfType<PlayerText>();
         perdiste.SetActive(false);
         initialPosition = transform.position;        
         ShowLives.livesAmount = 3;
@@ -29,9 +31,11 @@ public class ChooseOption : MonoBehaviour
     {
         if (other.CompareTag("RightOption"))
         {
+            transform.position = initialPosition;
             RightAnswer();
         }else if (other.CompareTag("WrongOption"))
         {
+            transform.position = initialPosition;
             WrongAnswer();
         }
     }
@@ -42,19 +46,23 @@ public class ChooseOption : MonoBehaviour
 
     void RightAnswer()
     {
-        ShowScore.scoreValue += 10;
-        transform.position = initialPosition;
-
+        ShowScore.scoreValue += 10;        
+        nextQuestions = FindObjectOfType<PlayerText>();
+        nextQuestions.NextButton();
     }
 
     void WrongAnswer()
     {
-        ShowLives.livesAmount--;
-        transform.position = initialPosition;
+        ShowLives.livesAmount--;        
+        
         if (ShowLives.livesAmount <= 0)
         {
             KillPlayer();          
             
+        }
+        else
+        {
+            nextQuestions.NextButton();
         }
     }
     
@@ -62,6 +70,7 @@ public class ChooseOption : MonoBehaviour
 
     void KillPlayer()
     {
+        this.gameObject.SetActive(false);
         perdiste.SetActive(true);
         principal.SetActive(false);
         
